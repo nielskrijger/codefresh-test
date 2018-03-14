42c8311d83ed9ddcf3a4a3cb2e077843f916aa80
Niels Krijger <niels@kryger.nl>
2018-03-14 06:42:44 +0100

    test: fix test composition candidate

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const catchError = require('./utils/catchError');
 
 const redis = new Redis({
   port: 6379,
-  host: '127.0.0.1'
+  host: process.env.REDIS_HOST || '127.0.0.1'
 });
 const app = express();
 
