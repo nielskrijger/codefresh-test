224e930fa62a2297567ac87d9c52891875257337
Niels Krijger <niels@kryger.nl>
2018-03-15 08:47:11 +0100

    test: improve test descriptions

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,20 +2,18 @@ const { assert } = require('chai');
 const shortid = require('shortid');
 const { get, post } = require('./helpers');
 
-describe('Set and retrieve redis key', () => {
-  it('should return 200 and available status', async () => {
-    const id = shortid.generate();
-    await post(`/redis/${id}`, { text: 'Hello!' }).expect(204);
+it('should store and retrieve a redis key using the REST api', async () => {
+  const id = shortid.generate();
+  await post(`/redis/${id}`, { text: 'Hello!' }).expect(204);
 
-    const { body } = await get(`/redis/${id}`).expect(200);
-    assert.equal(body.text, 'Hello!');
-  });
+  const { body } = await get(`/redis/${id}`).expect(200);
+  assert.equal(body.text, 'Hello!');
+});
 
-  it('should fail if FAIL=true', () => {
-    assert.notEqual(process.env.FAIL, 'true');
-  });
+it('should fail if FAIL=true', () => {
+  assert.notEqual(process.env.FAIL, 'true');
+});
 
-  it('should return 200 on healthcheck url', async () => {
-    await get('/healthcheck').expect(200);
-  });
+it('should return 200 on healthcheck url', async () => {
+  await get('/healthcheck').expect(200);
 });
